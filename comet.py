f090006bbd66708f099dbd9dce7ebdeb862096b0
YakuninAlexander <wepqwppw@gmail.com>
2022-12-20 19:54:16 +0300

    comet

--- comet.py
+++ comet.py
@@ -1,61 +1,72 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-G=6.674184*(10**-11)
-M=1.9891*(10**16)
+# Инициализация констант
+G = 6.674184 * (10 ** -11)
+M = 1.9891 * (10 ** 16)
 x0 = 100
 h = 0.001
-time=np.arange(0,10,h)
+time = np.arange(0, 10, h) # моменты времени полета кометы
 
 def func(t, r):
-    x, z, y, z1 = r
-    R = np.hypot(x,y)
-    fx = z
-    fz = -(G*M)*(x/R**3)
-    fy = z1
-    fz1 = -(G*M)*(y/R**3)
+    x, derX, y, derY = r # derX - производная X, derY - производная Y
+    R = np.hypot(x, y) # корень суммы квадратов x,y
+    der2X = -(G * M) * (x / R**3) # Вторые производные
+    der2Y = -(G * M) * (y / R**3)
 
-    return np.array([fx, fz, fy, fz1], float)
+    return np.array([derX, der2X, derY, der2Y], float)
 
-def r_init(a, v0):
-    y0 = 0
-    fx0 = v0*np.cos(np.radians(a))
-    fy0 = v0*np.sin(np.radians(a))
-    return np.array([x0, fx0, y0, fy0], float)
+def r_init(a, v0): # Инициализация начальных условий
+    fx0 = v0 * np.cos(np.radians(a))
+    fy0 = v0 * np.sin(np.radians(a))
 
-def runge_kutt(t, r):
-    k1 = func(t, r)
-    prom = [None] * 4
-    prom = r + k1*(h/2.0)
-    k2 = func(t + h/2.0, prom)
-    prom = r + k2*(h/2.0)
-    k3 = func(t+h/2.0, prom)
-    prom = r + k3*h
-    k4 = func(t+h, prom)
-    r+=(h/6.0)*(k1 + 2.0*k2 + 2.0*k3 + k4)
+    return np.array([x0, fx0, 0, fy0], float) # начальные условия
+
+def runge_kutt(t, r): # Метод Рунге-Кутта 4 порядка
+    k1 = func(t, r) # Коэффициенты метода
+    prom = r + k1 * (h / 2.0)
+    k2 = func(t + h / 2.0, prom)
+    prom = r + k2 * (h / 2.0)
+    k3 = func(t + h / 2.0, prom)
+    prom = r + k3 * h
+    k4 = func(t + h, prom)
+
+    r += (h / 6.0) * (k1 + 2.0 * k2 + 2.0 * k3 + k4) # Результат для времени t
+
     return r
 
-def pos(a, v0):
-    xpositions=[]
-    ypositions=[]
-    val_x=[]
-    val_y=[]
-    r = r_init(a, v0)
+def getCometPosition(angle, v0):
+    xpositions = []
+    ypositions = []
+
+    r = r_init(angle, v0)
+
     for t in time:
         r = runge_kutt(t, r)
-        xpositions.append(r[0])
-        val_x.append(r[1])
-        ypositions.append(r[2])
-        val_y.append(r[3])
-    return xpositions, ypositions, val_x, val_y
+        xpositions.append(r[0]) # Координата кометы х
+        ypositions.append(r[2]) # Координата кометы у
 
-x_pos, y_pos, val_x, val_y = pos(30, 129.2)
-fig = plt.figure()
-graf = fig.add_subplot(111)
-graf.plot(x_pos, y_pos)
-c = plt.Circle((0,0), 1, color = 'yellow')
-plt.gca ().add_artist (c)
-plt.show()
+    return xpositions, ypositions
 
+def UI():
+    while True:
+        print('1. Закрутить орбиту.')
+        print('2. Определить орбиту кометы для заданной скорости.')
+        ans = (int)(input('3. Выход.\n'))
+        if (ans == 1):
+            x_pos, y_pos = getCometPosition(30, 129.2)
+        elif (ans == 2):
+            v0 = (float)(input('Введите начальную скорость кометы: '))
+            x_pos, y_pos = getCometPosition(30, v0)
+        else:
+            return
 
+        fig = plt.figure()
+        graf = fig.add_subplot(111)
+        graf.plot(x_pos, y_pos)
 
+        c = plt.Circle((0,0), 1, color = 'red')
+        plt.gca().add_artist(c)
+        plt.show()
+
+UI()
